43b58a38bbb554e00294d0918fda9980e932120c
NikodemP <166309@student.uwm.edu.pl>
2022-03-02 09:03:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# a = 4
+# b = 6
+# if a > b:
+#    print("Liczba a jest wieksza")
+# elif a < b:
+#    print("Liczba b jest wieksza")
+# else:
+#    print("Liczby sa rowne")
+
+# a = input("Podaj liczbe: ")
+# print(a)
+# print(type(a))
+# a = int(a)
+# print(type(a))
+
+a = int(input("Podaj liczbe a: "))
+b = int(input("Podaj liczbe b: "))
+c = int(input("Podaj liczbe c: "))
+d = int(input("Podaj liczbe d: "))
+
+if (a > b) & (c > d):
+    print("Liczba a > b i c > d")
+else:
+    print("Liczba a < b lub c < d")
+
