bdfa4d974e79998488d26f1b9b5a6b6fc4ba3d23
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-03-16 08:53:42 +0100

    Update main.py

--- LAB 3/main.py
+++ LAB 3/main.py
@@ -2,5 +2,30 @@ a = []
 
 for x in range(10):
     a.append(x**2)
-
 print(a)
+
+a2 = [x**2 for x in range(10)]
+print(a)
+
+# ==================================
+
+b =[]
+
+for x in range(6):
+    b.append(3**x)
+print(b)
+
+b2 = [3**x for x in range(6)]
+print(b2)
+
+# ==================================
+
+c = []
+for x in a:
+    if x % 2 == 1:
+        c.append(x)
+print(c)
+
+c2 = [x for x in a if x % 2 == 1]
+print(c2)
+
