457459d1d3359a006fff8d5bfe4db6bad29782f7
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-03-16 10:09:37 +0100

    Update main2.py

--- LAB 3/main2.py
+++ LAB 3/main2.py
@@ -16,8 +16,8 @@ def rownanie_kwadratowe(a, b, c):
         x2 = (-b+math.sqrt(delta))/(2*a)
         return x1, x2
 
-def czy_parzysta(aLiczba):
-    if aLiczba % 2 == 0:
+def czy_parzysta(liczba):
+    if liczba % 2 == 0:
         print("Parzysta")
         return 1
     else:
@@ -26,6 +26,22 @@ def czy_parzysta(aLiczba):
 
 def dlugosc_odcinka(x1 = 0, y1 =0, x2 = 0, y2 = 0):
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def suma_ciagu(*ciag):
+    if len(ciag) == 0:
+        return 0
+    else:
+        suma = 0
+        for i in ciag:
+            suma += i
+        return suma
+
+def co_lubie(**rzeczy):
+    for cos in rzeczy:
+        print("to jest")
+        print(cos)
+        print("co lubie")
+        print(rzeczy[cos])
 
 print(rownanie_kwadratowe(6, 1, 3))
 print(rownanie_kwadratowe(1, 2, 1))
@@ -39,3 +55,8 @@ print(dlugosc_odcinka(1,2,2,2))
 print(dlugosc_odcinka(2,2, y2 = 2, x2 = 1))
 print(dlugosc_odcinka(y1 = 2, x1 = 5, y2 = 5, x2 = 3))
 print(dlugosc_odcinka(1,1))
+print("=======================")
+print(suma_ciagu())
+print(suma_ciagu(2,5,8,2,5,6,5,3))
+print("=======================")
+co_lubie(slodycze='czekolada', gry=["lol", "cs"])
