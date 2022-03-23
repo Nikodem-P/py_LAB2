0cee40bde96f8aa6469a53292e4bcaebbf77b364
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-03-23 09:58:13 +0100

    Update main.py

--- LAB 5/main.py
+++ LAB 5/main.py
@@ -1,8 +1,8 @@
 class Kształty():
 #definicja konstruktora
     def __init__(self, x, y):
-#deklarujemy atrybuty
-#self wskazuje że chodzi o zmienne własnie definiowanej klasy
+    #deklarujemy atrybuty
+    #self wskazuje że chodzi o zmienne własnie definiowanej klasy
         self.x = x
         self.y = y
         self.opis = "To jest klasa dla ogólnych kształtów"
@@ -26,11 +26,11 @@ class Kwadrat(Kształty):
     def __init__(self,x):
         self.x = x
         self.y = x
-# i jeszcze klasa, która dziedziczy po klasie Kwadrat
-# bedzie definiwoać figurę złożoną z 3 kwadratów w kształcie litery L
-#  _
-# | |_
-# |_ _|
+        # i jeszcze klasa, która dziedziczy po klasie Kwadrat
+        # bedzie definiwoać figurę złożoną z 3 kwadratów w kształcie litery L
+        #  _
+        # | |_
+        # |_ _|
 
 
 class KwadratLiteraL(Kwadrat):
@@ -42,7 +42,6 @@ class KwadratLiteraL(Kwadrat):
 
 #inicjujemy klasę Kwadrat
 kwadrat = Kwadrat(5)
-
 #sprawdzenie metod z klasy bazowej
 print(kwadrat.obwod())
 print(kwadrat.pole())
@@ -51,7 +50,6 @@ print(kwadrat.opis)
 kwadrat.skalowanie(0.3)
 print(kwadrat.obwod())
 print("")
-
 #inicjujemy klasę KwadratLiteraL
 litera_l = KwadratLiteraL(5)
 print(litera_l.obwod())
@@ -61,3 +59,77 @@ print(litera_l.opis)
 litera_l.skalowanie(0.5)
 print(litera_l.obwod())
 
+
+class Kwadrat(Kształty):
+    def __init__(self,x):
+        self.x = x
+        self.y = x
+        kwadrat = Kwadrat(5)
+        print(kwadrat)
+
+
+class Kwadrat(Kształty):
+    def __init__(self,x):
+    self.x = x
+    self.y = x
+
+    def __str__(self):
+        return 'Kwadrat o boku {}'.format(self.x)
+        kwadrat = Kwadrat(5)
+        print(kwadrat)
+
+
+class Osoba:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+
+    def przedstaw_sie(self):
+        return "{} {}".format(self.imie, self.nazwisko)
+
+
+class Pracownik(Osoba):
+    def __init__(self, imie, nazwisko, pensja):
+    # wywołanie konstruktora klasy bazowej
+    Osoba.__init__(self, imie, nazwisko)
+    # lub drugi sposób
+    # super().__init__(imie, nazwisko)
+    self.pensja = pensja
+
+    def przedstaw_sie(self):
+        return "{} {} i zarabiam {}".format(self.imie, self.nazwisko, self.pensja)
+
+
+class Menadzer(Pracownik):
+    def przedstaw_sie(self):
+        return "{} {}, jestem menadżerem i zarabiam {}".format(self.imie, self.nazwisko, self.pensja)
+
+jozek = Pracownik('Józef', 'Bajka', 2000)
+adrian = Menadzer('Adrian', 'Mikulski', 12000)
+print(jozek.przedstaw_sie())
+print(adrian.przedstaw_sie())
+
+class Osoba:
+    def __init__(self, imie, nazwisko):
+        self.imie = imieself.nazwisko = nazwisko
+
+    def przedstaw_sie(self):
+        return "{} {}".format(self.imie, self.nazwisko)
+
+
+class Pracownik:
+    def __init__(self, pensja):
+        self.pensja = pensja
+
+
+class Menadzer(Osoba, Pracownik):
+        def __init__(self, imie, nazwisko, pensja):
+            Osoba.__init__(self, imie,nazwisko)
+            Pracownik.__init__(self, pensja)
+
+        def przedstaw_sie(self):
+            return "{} {}, jestem menadżerem i zarabiam {}".format(self.imie, self.nazwisko, self.pensja)
+
+
+adrian = Menadzer("Adrian", "Mikulski", 12000)
+print(adrian.przedstaw_sie())
