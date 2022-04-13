6f9241d30d17168f77a9cc813cf34790c00a45e3
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-13 09:40:21 +0200

    Update and rename LAB8/main.py to LAB 8/main.py
    Zadanie 1 i 2

---
+++ LAB 8/main.py
@@ -0,0 +1,18 @@
+import numpy
+
+# Zadanie 1
+matrix1 = numpy.array(3)
+matrix2 = numpy.array(3)
+print(matrix1 * matrix2)
+
+# Zadanie 2
+matrix3 = numpy.arange(9).reshape((3, 3))
+print(matrix3)
+print('Wartosci maksymalne w kolumnach:')
+print(matrix3.min(axis=0))
+print('Wartosci maksymalne w wierszach:')
+print(matrix3.min(axis=1))
+print('Wartosci minimalne w kolumnach:')
+print(matrix3.max(axis=0))
+print('Wartosci minimalne w wierszach:')
+print(matrix3.max(axis=1))
