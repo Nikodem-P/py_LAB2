844e8f7cc7c383a5437252a12c83472c6ff1f334
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-13 09:52:26 +0200

    Update main.py

--- LAB 8/main.py
+++ LAB 8/main.py
@@ -1,12 +1,13 @@
 import numpy
 
 # Zadanie 1
-matrix1 = numpy.array(3)
-matrix2 = numpy.array(3)
+matrix1 = numpy.arange(3)
+matrix2 = numpy.arange(3)
 print(matrix1 * matrix2)
+print('===============')
 
 # Zadanie 2
-matrix3 = numpy.arange(9).reshape((3, 3))
+matrix3 = numpy.arange(9).reshape(3, 3)
 print(matrix3)
 print('Wartosci maksymalne w kolumnach:')
 print(matrix3.min(axis=0))
@@ -20,3 +21,33 @@ print('===============')
 
 # Zadanie 3
 print(matrix1.dot(matrix2))
+print('===============')
+
+# Zadanie 4
+matrix4 = numpy.arange(3, dtype='int64')
+matrix5 = numpy.array([[4.6, 7.9, 2.3]])
+print(matrix4 * matrix5)
+print('===============')
+
+# Zadanie 5
+matrix6 = numpy.arange(6).reshape(2, 3)
+a = numpy.sin(matrix6)
+print(a)
+print('===============')
+
+# Zadanie 6
+matrix7 = numpy.arange(10, 22, 2).reshape(2, 3)
+b = numpy.cos(matrix7)
+print(b)
+print('===============')
+
+# Zadanie 7
+print(a + b)
+
+# Zadanie 8
+matrix8 = numpy.arange(9).reshape(3,3)
+for i in matrix8:
+    print(i)
+    
+# Zadanie 9
+matrix9 = 
