3bb307bdefef26143d63dd881e151261bb219234
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-20 09:54:24 +0200

    Update main.py

--- LAB 9/main.py
+++ LAB 9/main.py
@@ -4,8 +4,10 @@ import pandas
 s1 = pandas.Series([1, 3, 5.5, numpy.nan, 'a'])
 print(s1)
 
+
 s2 = pandas.Series([10, 12, 8, 14], index=['a','b','c','d'])
 print(s2)
+
 
 dane = {
     "Kraj" : ["Belgia", "Indie", "Brazylia"],
@@ -15,11 +17,13 @@ dane = {
 df = pandas.DataFrame(dane)
 print(df)
 
+
 daty = pandas.date_range('20220420', periods=5)
 print(daty)
 
 df2 = pandas.DataFrame(numpy.random.randn(5, 4), index=daty, columns=list('ABCD'))
 print(df2)
+
 
 iris = pandas.read_csv("iris.csv", header=0, sep=',', decimal='.')
 print(iris)
@@ -32,6 +36,7 @@ print(df3)
 
 df3.to_excel('nowy.xlsx', sheet_name="Arkusz 1", index=False)
 print("============")
+
 print(s2['a'])
 print(s2.a)
 print(df["Stolica"])
@@ -42,4 +47,20 @@ print("Kraj: " + df.Kraj)
 print(iris.sample(5))
 print(iris.sample(frac=0.05))
 print(iris.sample(10, replace=True))
-print(df.head())
+print(iris.head())
+print(iris.tail())
+
+
+print(s2[(s2 > 9)])
+
+print(s2.where(s2 > 10, "element za maly"))
+seria = s2.copy()
+seria.where(seria <= 10, "element za duzy", inplace=True)
+print(seria)
+print(s2[(s2 < 13) & (s2 > 8)])
+
+
+print(df[df["Populacja"] > 12000000])
+print(df[((df.Populacja > 1000000) & (df.index.isin([0, 2])))])
+szukaj = ["Belgia", "Brasilia"]
+print(df.isin(szukaj))
