e2aabce6d762230c3f5a39238735af59ce865e82
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-04-27 09:16:45 +0200

    Commit 9:16

--- LAB_10/main.py
+++ LAB_10/main.py
@@ -1,2 +1,32 @@
 import pandas
+import numpy
+from matplotlib import pyplot
+
+# ts0 = pandas.Series(numpy.random.randn(1000))
+#
+# ts0 = ts0.cumsum()
+# print(ts0)
+# ts0.plot()
+#
+# pyplot.savefig("wykres0.png")
+# pyplot.show()
+
+kraje = {
+    "Kraj" : ["Belgia", "Indie", "Brazylia", "Polska"],
+    "Stolica" : ["Bruksela", "New Delphi", "Brasilia", "Warszawa"],
+    "Populacja" : [11190846, 1303171035, 207347528, 38675467],
+    "Kontynent" : ["Europa", "Azja", "Ameryka Południowa", "Europa"]}
+
+data = pandas.DataFrame(kraje)
+print(data)
+
+grupa = data.groupby("Kontynent").agg({"Populacja": ["sum"]})
+# grupa.plot(kind="bar", xlabel="Kontynent", ylabel="Populacja (mld)", rot=0, title="Populacja na kontynentach")
+wykres = grupa.plot.bar()
+wykres.set_xlabel("Kontyenty")
+wykres.set_ylabel("Populacja (mld)")
+wykres.tick_params(axis='x', labelrotation=0)
+wykres.legend()
+pyplot.savefig("wykres1.png")
+pyplot.show()
 
